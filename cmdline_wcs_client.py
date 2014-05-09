22a6b0076595e31900a45a123d24b40ae27d9847
christian.schiller <christian.schiller@eox.at>
2014-05-09 12:15:08 +0200

    cmdline_wcs_client.py - now containing the cmd-line interface to the wcsClient, requires wcs_client to be imported

--- cmdline_wcs_client.py
+++ cmdline_wcs_client.py
@@ -2,25 +2,89 @@
 
 # -*- coding: utf-8 -*-
 """
-Created on Thu May  8 21:22:54 2014
+#------------------------------------------------------------------------------
+# Name:  cmdline_wcs_client.py
+#
+#   Command Line interface to the general purpose WCS 2.0/EO-WCS Client (wcs_client.py):
+#       This routine allows to use the  general purpose WCS 2.0/EO-WCS Client (wcs_client.py)
+#       from the cmd-line. It importes the wcs_client (required) as a module.
+#       It provieds extensive help information.
+#
+#   This WCS-Client provides the following functionality:
+#         - GetCapabilities Request
+#         - DescribeCoverage Request
+#         - DescribeEOCoverageSet Request
+#         - GetMap Request
+#
+#         - return responses
+#         - download coverages
+#         - download time-series of coverages
+#
+#   It allows users to specify:
+#         + Server URL
+#         + Area of Interest (subset)
+#         + Time of Interest (time constrain)
+#         + DatasetSeries or Coverage
+#         + Rangesubsetting (eg. Bands)
+#         + File-Format (image format) for downloads
+#         + output CRS for downloads
+#         + mediatype
+#         + updateSequence
+#         + containment
+#         + section
+#         + count
+#         + interpolation
+#         + size or resolution
+#
+#   Additional (non-standard parameters implemented:
+#           + mask
+#           + IDs_only (DescribeEOCoverageSet returns only CoverageIDs - to be used for immediate download)
+#           + output (GetCoverage only - local location where downloaded files shall be written too)
+#
+#
+#
+# Name:        cmdline_wcs_client.py
+# Project:     DeltaDREAM
+# Author(s):   Christian Schiller <christian dot schiller at eox dot at>
+##
+#-------------------------------------------------------------------------------
+# Copyright (C) 2014 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
 
-@author: schillerc
 """
 
 import sys
-#import os
+
 import argparse
-import wcs_client #as wcsClient
+    # wcs_client provides the WCS/EO-WCS functionality (required)
+import wcs_client   # as wcsClient
 
 global __version__
-__version__ = '0.1-beta'
+__version__ = '0.1'
 
 
 
 #/************************************************************************/
 #/*                              _get_cmdline()                           */
 #/************************************************************************/
-#def _get_cmdline(args):
 def _get_cmdline():
     """
         Handles the input from the cmd-line, extensive help, and usage information.
@@ -65,9 +129,6 @@ def _get_cmdline():
             checks and reformats the recieved coordinates and provides the corrcet Axis syntax
         """
         def __call__(self, cl_parser, namespace, values, option_string=None):
-            print 'VVVV',values
-            print option_string
-            print cl_parser
             print namespace
             if values[0].startswith('epsg'):
                 crs = values[0].split(':')[1]
@@ -94,8 +155,6 @@ def _get_cmdline():
                 coord = values[1].split(',')
             else:
                 coord = values[0].split(',')
-
-            print coord
 
             if  len(coord) % 2 is 0:
                 if coord[0] == coord[-2] and coord[1] == coord[-1]:
@@ -277,8 +336,6 @@ def _get_cmdline():
                         epsg:4326 42,10,43,12,39,13,38,9,42,10')
 
         # parse the input and convert it into a dict
-    #u_input = cl_parser.parse_args()
-    #input_params = u_input.__dict__
     input_params = cl_parser.parse_args().__dict__
 
     return input_params
@@ -288,13 +345,13 @@ def _get_cmdline():
 #/************************************************************************/
 #/*                               main()                                 */
 #/************************************************************************/
-#def main(args):
 def main():
     """
-        Main function of:
-        General purpose WCS client for WCS 2.0/EO-WCS server access.
-        This client provides all functionalities as described in the EOxServer ver.0.3
-        documentation (http://eoxserver.org/doc/en/users/EO-WCS_request_parameters.html)
+        Command Line interface to the general purpose WCS client (wcs_client.py) 
+        for WCS 2.0/EO-WCS server access
+        This cmd-line interface handles all functionalities as described in 
+        the EOxServer ver.0.3 documentation 
+        (http://eoxserver.org/doc/en/users/EO-WCS_request_parameters.html)
         It offers cmd-line execution of:
           - GetCapabilities Request
           - DescribeCoverage Request
@@ -304,22 +361,18 @@ def main():
    # print "I'm in "+sys._getframe().f_code.co_name
 
         # get all parameters provided via cmd-line
-    #input_params = _get_cmdline(args)
     input_params = _get_cmdline()
 
         # execute the user selected Request-type
     if input_params.has_key('request'):
         to_call = input_params.get('request')
-#        print 'Request', to_call
 
     wcs_call = wcs_client.wcsClient()
 
     exec "result = wcs_call."+to_call+"(input_params)"
 
-
     print result
 
 
 if __name__ == "__main__":
-    #sys.exit(main(sys.argv))
     sys.exit(main())
